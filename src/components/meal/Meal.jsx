5252413106b51fce0eaf0ad5ad6105d2a6807832
Ainazik Momunalieva <58284183+ainaziko@users.noreply.github.com>
2024-06-09 19:06:15 +0600

    wrote queries to retrieve a random dish from API

--- src/components/meal/Meal.jsx
+++ src/components/meal/Meal.jsx
@@ -1,29 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
 import classes from './Meal.module.css'
+import { Link } from 'react-router-dom'
+const src  = 'https://www.themealdb.com/api/json/v1/1/random.php';
+  
 
-const Meal = ({ headingTxt, mealName, mealCategory, mealArea, mealImgUrl }) => {
-    const [meal, setMeal] = useState({
-        headingTxt,
-        mealName,
-        mealCategory,
-        mealArea,
-        mealImgUrl
-    });
+const Meal = ({ headingTxt }) => {
+    const [meal, setMeal] = useState(null);
 
+    useEffect(() => {
+        axios
+        .get(src) 
+        .then(response => {
+            setMeal(response.data.meals[0]);
+            console.log(response.data.meals[0]);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    }, [])
+ 
+    if (!meal) {
+        return <p>Loading...</p>;
+    }
+    
     return (
         <section className={classes.meal}>
             <div className={classes.meal__info}>
-                <p className={classes.meal__heading}>{meal.headingTxt}</p>
-                <a className={classes.meal__name} href="">{meal.mealName}</a>
+                <p className={classes.meal__heading}>{headingTxt}</p>
+                <Link className={classes.meal__name} key={meal.mealId} to={'/${meal.mealId}'}>{meal.strMeal}</Link>
                 <p>
-                    <span className={classes.meal__category}>{meal.mealCategory}</span> | <span className={classes.meal__area}>{meal.mealArea}</span>
+                    <span className={classes.meal__category}>{meal.strCategory}</span> | <span className={classes.meal__area}>{meal.strArea}</span>
                 </p>
             </div>
             <div className={classes.meal__media}>
-                <img className={classes.meal__img} src={meal.mealImgUrl} alt={meal.mealName}/>
+                <img className={classes.meal__img} src={meal.strMealThumb} alt={meal.strMeal}/>
             </div>
         </section>
     );
 }
 
 export default Meal;
+
